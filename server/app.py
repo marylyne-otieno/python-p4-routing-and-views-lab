a2495218a88c8cad63e57f6d04db5f4fae514d0e
marylyne <otienomarylyne76@gmail.com>
2025-06-10 07:11:26 +0300

    adds sever

--- server/app.py
+++ server/app.py
@@ -3,6 +3,37 @@
 from flask import Flask
 
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return(parameter)
+@app.route('/count/<int:num>')
+def count(num):
+    result = ''
+    for i in range(num):
+        result += f'{i}\n'
+    return result
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            return 'Division by zero error'
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Invalid operation'
+
+    return str(result)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
